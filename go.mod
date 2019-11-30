d7d5d2a445a653a5455ab6b4086c7059286f8eda
Jordan Halterman <jordan.halterman@gmail.com>
2019-11-29 16:37:46 -0800

    Use log slot num in client responses to ensure consistency for streaming responses

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.12
 
 require (
 	github.com/atomix/atomix-api v0.0.0-20191021183656-837bfda65c82
-	github.com/atomix/atomix-go-node v0.0.0-20191021234659-c841a97bec89
-	github.com/atomix/atomix-nopaxos-node v0.0.0-20191127201526-d45c3ef906d7
+	github.com/atomix/atomix-go-node v0.0.0-20191130003522-53d52c9deb5c
+	github.com/atomix/atomix-nopaxos-node v0.0.0-20191130003641-1e2faf108d01
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/mock v1.3.1
--- pkg/atomix/nopaxos/client.go
+++ pkg/atomix/nopaxos/client.go
@@ -22,6 +22,7 @@ import (
 	"github.com/atomix/atomix-nopaxos-node/pkg/atomix/nopaxos/protocol"
 	"github.com/atomix/atomix-nopaxos-node/pkg/atomix/nopaxos/util"
 	"math"
+	"sort"
 	"sync"
 	"time"
 )
@@ -36,6 +37,10 @@ func NewClient(config cluster.Cluster, sequencerConfig *SequencerConfig) (*Clien
 // newClient returns a new NOPaxos client
 func newClient(cluster *Cluster, config *SequencerConfig) (*Client, error) {
 	quorum := int(math.Floor(float64(len(cluster.Members()))/2.0)) + 1
+	acks := make(map[protocol.MemberID]protocol.LogSlotID)
+	for _, member := range cluster.Members() {
+		acks[protocol.MemberID(member)] = 0
+	}
 	client := &Client{
 		logger:     util.NewNodeLogger("sequencer"),
 		cluster:    cluster,
@@ -43,6 +48,7 @@ func newClient(cluster *Cluster, config *SequencerConfig) (*Client, error) {
 		writeChans: list.New(),
 		readChans:  list.New(),
 		quorum:     quorum,
+		acks:       acks,
 		commands:   make(map[protocol.MessageID]*commandHandler),
 		queries:    make(map[protocol.MessageID]*queryHandler),
 		writes:     make(chan *requestContext, queueSize),
@@ -66,6 +72,7 @@ type Client struct {
 	quorum     int
 	commandID  protocol.MessageID
 	queryID    protocol.MessageID
+	acks       map[protocol.MemberID]protocol.LogSlotID
 	commands   map[protocol.MessageID]*commandHandler
 	queries    map[protocol.MessageID]*queryHandler
 	writes     chan *requestContext
@@ -106,10 +113,41 @@ func (c *Client) receive(member MemberID, stream protocol.ClientService_ClientSt
 				continue
 			}
 			c.logger.ReceiveFrom("CommandReply", r.CommandReply, member)
+
 			c.mu.Lock()
+
+			// Record the slot ack
+			c.acks[r.CommandReply.Sender] = r.CommandReply.SlotNum
+
+			// Compute the new commit slot
+			slots := make([]protocol.LogSlotID, 0, len(c.acks))
+			for _, slotNum := range c.acks {
+				slots = append(slots, slotNum)
+			}
+			sort.Slice(slots, func(i, j int) bool {
+				return slots[i] < slots[j]
+			})
+			commitSlotNum := slots[c.quorum-1]
+
+			// Get the command reply handler
 			handler := c.commands[r.CommandReply.MessageNum]
-			if handler != nil && handler.succeed(r.CommandReply.Value) {
-				delete(c.commands, r.CommandReply.MessageNum)
+			if handler != nil {
+				// If a reply value was returned, set the value
+				if r.CommandReply.Value != nil {
+					handler.receive(r.CommandReply.SlotNum, r.CommandReply.Value)
+				}
+				handler.commit(commitSlotNum)
+			}
+			c.mu.Unlock()
+		case *protocol.ClientMessage_CommandClose:
+			if r.CommandClose.ViewID.SessionNum != c.config.SessionId {
+				continue
+			}
+			c.logger.ReceiveFrom("CommandClose", r.CommandClose, member)
+			c.mu.Lock()
+			handler := c.commands[r.CommandClose.MessageNum]
+			if handler != nil {
+				delete(c.commands, r.CommandClose.MessageNum)
 				handler.complete()
 			}
 			c.mu.Unlock()
@@ -118,10 +156,23 @@ func (c *Client) receive(member MemberID, stream protocol.ClientService_ClientSt
 				continue
 			}
 			c.logger.ReceiveFrom("QueryReply", r.QueryReply, member)
+			if r.QueryReply.Value != nil {
+				c.mu.Lock()
+				handler := c.queries[r.QueryReply.MessageNum]
+				if handler != nil {
+					handler.receive(r.QueryReply.Value)
+				}
+				c.mu.Unlock()
+			}
+		case *protocol.ClientMessage_QueryClose:
+			if r.QueryClose.ViewID.SessionNum != c.config.SessionId {
+				continue
+			}
+			c.logger.ReceiveFrom("QueryReply", r.QueryClose, member)
 			c.mu.Lock()
-			handler := c.queries[r.QueryReply.MessageNum]
-			if handler != nil && handler.succeed(r.QueryReply.Value) {
-				delete(c.queries, r.QueryReply.MessageNum)
+			handler := c.queries[r.QueryClose.MessageNum]
+			if handler != nil {
+				delete(c.queries, r.QueryClose.MessageNum)
 				handler.complete()
 			}
 			c.mu.Unlock()
@@ -151,7 +202,7 @@ func (c *Client) Read(ctx context.Context, in []byte, ch chan<- node.Output) err
 
 func (c *Client) processWrites() {
 	for context := range c.writes {
-		handler := &commandHandler{ch: context.ch, quorum: c.quorum}
+		handler := c.newCommandHandler(context.ch)
 		messageID := c.commandID + 1
 		c.commandID = messageID
 		c.mu.Lock()
@@ -231,53 +282,69 @@ type requestContext struct {
 	ch    chan<- node.Output
 }
 
+func (c *Client) newCommandHandler(ch chan<- node.Output) *commandHandler {
+	return &commandHandler{
+		values: make(map[protocol.LogSlotID]*list.List),
+		ch:     ch,
+	}
+}
+
 // commandHandler is a quorum reply handler
 type commandHandler struct {
-	quorum    int
-	succeeded int
-	failed    int
-	value     []byte
-	ch        chan<- node.Output
+	values map[protocol.LogSlotID]*list.List
+	ch     chan<- node.Output
 }
 
-func (h *commandHandler) succeed(value []byte) bool {
-	if h.value == nil && len(value) > 0 {
-		h.value = value
+func (h *commandHandler) receive(slotNum protocol.LogSlotID, value []byte) {
+	values := h.values[slotNum]
+	if values == nil {
+		values = list.New()
+		h.values[slotNum] = values
 	}
-	h.succeeded++
-	return h.value != nil && h.succeeded >= h.quorum
+	values.PushBack(value)
 }
 
-func (h *commandHandler) fail() bool {
-	h.failed++
-	return h.failed >= h.quorum
+func (h *commandHandler) commit(slotNum protocol.LogSlotID) {
+	slots := make([]protocol.LogSlotID, 0, len(h.values))
+	for slot := range h.values {
+		slots = append(slots, slot)
+	}
+	sort.Slice(slots, func(i, j int) bool {
+		return slots[i] < slots[j]
+	})
+	for _, slot := range slots {
+		if slot > slotNum {
+			return
+		}
+		values := h.values[slot]
+		element := values.Front()
+		for element != nil {
+			h.ch <- node.Output{
+				Value: element.Value.([]byte),
+			}
+			element = element.Next()
+		}
+		delete(h.values, slot)
+	}
 }
 
 func (h *commandHandler) complete() {
-	h.ch <- node.Output{
-		Value: h.value,
-	}
+	close(h.ch)
 }
 
 // queryHandler is a query reply handler
 type queryHandler struct {
-	ch    chan<- node.Output
-	value []byte
+	ch chan<- node.Output
 }
 
-func (h *queryHandler) succeed(value []byte) bool {
-	h.value = value
-	return h.value != nil
-}
-
-func (h *queryHandler) fail() bool {
-	return true
+func (h *queryHandler) receive(value []byte) {
+	h.ch <- node.Output{
+		Value: value,
+	}
 }
 
 func (h *queryHandler) complete() {
-	h.ch <- node.Output{
-		Value: h.value,
-	}
+	close(h.ch)
 }
 
 // Close closes the client
